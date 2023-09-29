e25777d6eb09f7825699775bf15e288d800fbaf3
thanhnd4869 <thanhnd4869@gmail.com>
2023-09-29 21:47:37 +0700

    init app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by ThanhND
+ */
+import express from "express";
+
+const app = express();
+
+const hostname = "localhost";
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.end("<h1>Hello World!</h1><hr>");
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Hello, I am running at ${hostname}:${port}/`);
+});
